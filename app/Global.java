8f852140051bf3248fc19161c005d29a68041a45
wnalle <nalle@hawaii.edu>
2015-04-08 14:03:25 -1000

    Added name to local map

--- app/Global.java
+++ app/Global.java
@@ -19,13 +19,13 @@ public class Global extends GlobalSettings {
     ArrayList<TimedIngredients> ingredients = new ArrayList<TimedIngredients>();
     ingredients.add(new TimedIngredients("Kale", 10, Calendar.getInstance(), Calendar.getInstance(), 1.99));
     ingredients.add(new TimedIngredients("Orange", 5, Calendar.getInstance(), Calendar.getInstance(), 2.49));
-    Farmer farmer = new Farmer(1, "Nalo LIFE", "Farmer", "KCC Farmers Market", ingredients);
+    Farmer farmer = new Farmer(1, "Nalo LIFE", "Farmer", "4303 Diamond Head Rd, Honolulu, HI", ingredients);
     FarmerDB.addFarmer(farmer);
 
     ArrayList<TimedIngredients> ingredients2 = new ArrayList<TimedIngredients>();
     ingredients2.add(new TimedIngredients("Celery", 100, Calendar.getInstance(), Calendar.getInstance(), 0.50));
     ingredients2.add(new TimedIngredients("Carrots", 40, Calendar.getInstance(), Calendar.getInstance(), 5.00));
-    Farmer farmer2 = new Farmer(2, "Aloun Farms", "Farmer", "KCC Farmers Market", ingredients2);
+    Farmer farmer2 = new Farmer(2, "Aloun Farms", "Farmer", "1050 Ala Moana Blvd Honolulu, HI", ingredients2);
     FarmerDB.addFarmer(farmer2);
   }
 
